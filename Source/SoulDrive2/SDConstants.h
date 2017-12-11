c25f8c76f51a0132778efd0a3d7897423f7bc843
Peter Keyser <keysertheloner@gmail.com>
2017-12-11 13:21:35 -0500

    Added constants and some work on dynamic hotkeys

---
+++ Source/SoulDrive2/SDConstants.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class SOULDRIVE2_API SDConstants
+{
+public:
+	SDConstants();
+	~SDConstants();
+};
