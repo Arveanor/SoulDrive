57b8d59cb9a8a99639ff8b3f669c4ed142f65117
Peter Keyser <keysertheloner@gmail.com>
2018-04-18 21:53:11 -0400

    AI, Map Generation, ip address connection

---
+++ Source/SoulDrive2/SDSunBurstSpell.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "SDBaseSpell.h"
+#include "SDSunBurstSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOULDRIVE2_API ASDSunBurstSpell : public ASDBaseSpell
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/SoulDrive2/SDTeamIdentity.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SoulDrive2.h"
+#include "SDTeamIdentity.h"
+
+
+// This function does not need to be modified.
+USDTeamIdentity::USDTeamIdentity(const class FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+}
+
+// Add default functionality here for any ISDTeamIdentity functions that are not pure virtual.
