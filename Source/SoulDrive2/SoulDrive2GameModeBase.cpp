d03af94ef01f30e5ddd24e50fcbd90e37cceccea
Unknown <keysertheloner@gmail.com>
2018-09-27 21:10:25 -0400

    Created logical implementation of edge-alignment procgen algorithm

--- Source/SoulDrive2/SoulDrive2GameModeBase.cpp
+++ Source/SoulDrive2/SoulDrive2GameModeBase.cpp
@@ -38,29 +38,103 @@ AActor* ASoulDrive2GameModeBase::ChoosePlayerStart_Implementation(AController* P
 }
 
 
-void ASoulDrive2GameModeBase::GenerateMapData(UPARAM(ref) TArray<int> &TileList, FMapGenerationParams Params)
+void ASoulDrive2GameModeBase::GenerateMapData(UPARAM(ref) TArray<USDTileDescriptor*> &TileList, FMapGenerationParams Params)
 {
 	int RandomIndex = 0;
+	/* 
+	** Array of indices not yet overwritten by a "necessary" tile.  Will be filled in with all
+	** of the possible indices initially, and those elements will be removed when a needed tile is added on the second pass
+	*/
 	TArray<int> availableIndices;
-	TileList.SetNum(Params.TileCountX * Params.TileCountY);
+	TArray<USDTileDescriptor *> possibleTilesForMap; // 0 as nullptr
+	TSet<USDTileDescriptor *> candidateTiles;
+	TArray<ProceduralTileEdges> neighborEdges;
+	TArray<USDTileDescriptor *> neighborTiles;
+	TileList.Init(0, Params.TileCountX * Params.TileCountY);
 	availableIndices.SetNum(TileList.Num());
+	USDTileDescriptor* TileToAdd;
+	// Ok so until *after* we place a tile that is past the existing borders, we do not turn, that is why
+	// This is "previous" neighbors count, and not neighbors count. Please stop getting confused and changing it
+	int previousNeighborsCount = 0;
+	int currentDirection = 0; // consider 0 degree rotation to be up
+	int prevX = 0;
+	int prevY = 0;
+
+	// Loop over all locations to be filled in, spiral outward from the center point and add new Tiles that have edges that
+	// align with their already placed neighbors
+	for (int i = 0; i < TileList.Num(); i++)
+	{
+		previousNeighborsCount = 0; // reset to 0 so we can recalculate it for every tile
+
+		// Determine if we need to rotate our tile laying direction to keep the spiral going
+		if (previousNeighborsCount == 1)
+		{
+			currentDirection += 90;
+			currentDirection = currentDirection % 360;
+		}
+
+		// Update the (x, y) of our next tile based on our tile laying direction
+		switch (currentDirection)
+		{
+		case 0:
+			prevY += 1;
+			break;
+		case 90:
+			prevX += 1;
+			break;
+		case 180:
+			prevY -= 1;
+			break;
+		case 270:
+			prevX -= 1;
+		}
+
+		// put all of our 4 neighbors into a happy little array
+		neighborTiles[0] = TileList[IndexFromPoint(prevX, prevY + 1, Params.TileCountX, Params.TileCountY)];
+		neighborTiles[1] = TileList[IndexFromPoint(prevX + 1, prevY, Params.TileCountX, Params.TileCountY)];
+		neighborTiles[2] = TileList[IndexFromPoint(prevX, prevY - 1, Params.TileCountX, Params.TileCountY)];
+		neighborTiles[3] = TileList[IndexFromPoint(prevX - 1, prevY, Params.TileCountX, Params.TileCountY)];
+		for (int j = 0; j < 4; j++)
+		{
+			if (neighborTiles[j]->name != FName("EmptySpace")) // check if we have an already applied neighbor tile or just a placeholder
+			{
+				previousNeighborsCount++;
+			}
+			neighborEdges[j] = neighborTiles[j]->GetFacingEdge(j * 90); // we really care about which edge of our neighbor tile faces us, the rest is irrelevant!
+		}		
+
+		// Check every tile for this map for validity with our neighborEdges array, the ones that pass will be in a random roll to get chosen
+		candidateTiles.Reset();
+		for (USDTileDescriptor* Tile : Params.TileSet)
+		{
+			// We're going to make a new tile so that we can actually control rotation at each point
+			TileToAdd = NewObject<USDTileDescriptor>();
+			TileToAdd->CopyDescriptorData(candidateTiles.Array()[FMath::RandRange(0, candidateTiles.Num() - 1)]);
+			if (TileToAdd->isValidForNeighbors(neighborEdges))
+			{
+				candidateTiles.Add(TileToAdd);
+			}
+		}
+
+		TileList[IndexFromPoint(prevX, prevY, Params.TileCountX, Params.TileCountY)] = candidateTiles.Array()[FMath::RandRange(0, candidateTiles.Num() - 1)];
+	}
 
 	for (int i = 0; i < TileList.Num(); i++)
 	{
-		TileList[i] = Params.ActorIds[FMath::RandRange(0, Params.ActorIds.Num() - 1)];
+// 		TileList[i] = Params.ActorIds[FMath::RandRange(0, Params.ActorIds.Num() - 1)];
 		availableIndices[i] = i;
 	}
 
 	// Write over locations in tile list. Remove indices from availableIndices to avoid collisions
-	for (TPair<int, int> KeyFrequencyPair : Params.ExactlyNIds)
-	{
-		for (int j = 0; j < KeyFrequencyPair.Value; j++)
-		{
-			RandomIndex = FMath::RandRange(0, availableIndices.Num() - 1);
-			TileList[availableIndices[RandomIndex]] = KeyFrequencyPair.Key;
-			availableIndices.RemoveAt(RandomIndex);
-		}
-	}
+// 	for (TPair<int, int> KeyFrequencyPair : Params.ExactlyNIds)
+// 	{
+// 		for (int j = 0; j < KeyFrequencyPair.Value; j++)
+// 		{
+// 			RandomIndex = FMath::RandRange(0, availableIndices.Num() - 1);
+// 			TileList[availableIndices[RandomIndex]] = KeyFrequencyPair.Key;
+// 			availableIndices.RemoveAt(RandomIndex);
+// 		}
+// 	}
 }
 
 void ASoulDrive2GameModeBase::SpawnPlayerCharacter(APlayerController* Player) 
@@ -93,14 +167,9 @@ void ASoulDrive2GameModeBase::SpawnPlayerCharacter(APlayerController* Player)
 	}
 }
 
-void ASoulDrive2GameModeBase::BuildLevelMulticast_Implementation(FRandomStream Stream)
+int ASoulDrive2GameModeBase::IndexFromPoint(int x, int y, int maxX, int maxY)
 {
-	//GenerateMapData()
-}
-
-bool ASoulDrive2GameModeBase::BuildLevelMulticast_Validate(FRandomStream Stream)
-{
-	return true;
+	return x + y * maxY;
 }
 
 int32 ASoulDrive2GameModeBase::GetListKeyByIndex(TDoubleLinkedList<int32> &List, int32 Index)
@@ -119,4 +188,16 @@ int32 ASoulDrive2GameModeBase::GetListKeyByIndex(TDoubleLinkedList<int32> &List,
 		}
 	}
 	return result;
+}
+
+uint8 ASoulDrive2GameModeBase::GetEdgeMapIndex(ProceduralTileEdges inEdge, TArray<FEdgeAlignmentPair> Map)
+{
+	for (int i = 0; i < Map.Num(); i++)
+	{
+		if (Map[i].Key == inEdge)
+		{
+			return i;
+		}
+	}
+	return -1;
 }
